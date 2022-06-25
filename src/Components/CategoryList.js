2880216deec3827caa76c138121ee9a4b021069d
osmanbeshirov <osman.beshirov@gmail.com>
2022-06-25 22:59:39 +0400

    class type state is  used

--- src/Components/CategoryList.js
+++ src/Components/CategoryList.js
@@ -3,28 +3,32 @@ import React, { Component } from 'react'
 import { ListGroup, ListGroupItem } from 'reactstrap'
 
 export default class CategoryList extends Component {
+
+  state = {
+    categories: [
+      { categroyId: 1, categoryName: 'Beverages' },
+      { categroyId: 2, categoryName: 'Condiments' },
+      { categroyId: 3, categoryName: 'Beverages' },
+      { categroyId: 4, categoryName: 'Beverages' },
+      { categroyId: 5, categoryName: 'Beverages' },
+    ],
+
+  }
+
+
   render() {
     return (
       <div>
         <h3>{this.props.info.title}</h3>
-        <ListGroup
-          numbered
-        >
-          <ListGroupItem>
-            Cras justo odio
-          </ListGroupItem>
-          <ListGroupItem>
-            Dapibus ac facilisis in
-          </ListGroupItem>
-          <ListGroupItem>
-            Morbi leo risus
-          </ListGroupItem>
-          <ListGroupItem>
-            Porta ac consectetur ac
-          </ListGroupItem>
-          <ListGroupItem>
-            Vestibulum at eros
-          </ListGroupItem>
+
+        <h3>{this.state.counter}</h3>
+        <ListGroup numbered>
+          {this.state.categories.map((item) => (
+            <ListGroupItem key={item.categroyId}>
+              {item.categoryName}
+            </ListGroupItem>
+          ))}
+
         </ListGroup>
 
 
