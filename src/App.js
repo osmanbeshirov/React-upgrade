cc974b9826ee737198835c9cea3cba87e1de520b
osmanbeshirov <osman.beshirov@gmail.com>
2022-06-25 15:31:49 +0400

    components succesfully are created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import CategoryList from './Components/CategoryList';
+import Navbar from './Components/Navbar';
+import ProductList from './Components/ProductList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <CategoryList/>
+      <ProductList/>
     </div>
   );
 }
