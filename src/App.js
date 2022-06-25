d5c9e76d7a8491e577379f880636b955f18805d4
osmanbeshirov <osman.beshirov@gmail.com>
2022-06-25 15:55:35 +0400

    reactstrap successfully is worked

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
+import { Container, Row, Col } from 'reactstrap';
 import './App.css';
 import CategoryList from './Components/CategoryList';
 import Navbar from './Components/Navbar';
 import ProductList from './Components/ProductList';
 
+
+
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <CategoryList/>
-      <ProductList/>
+
+      <Container>
+        <Row>
+          <Navbar />
+        </Row>
+
+        <Row>
+          <Col xs='3'>
+            <CategoryList />
+          </Col>
+
+          <Col xs='9'>
+            <ProductList />
+          </Col>
+
+
+
+
+        </Row>
+
+      </Container>
+
+
     </div>
   );
 }
